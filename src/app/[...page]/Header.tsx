c16cb83c6593e10b7981f4c070d11db9da15bb90
Christian Jensen <cbejensen@gmail.com>
2023-03-08 11:28:49 -0700

    add imports for Route

--- src/app/[...page]/Header.tsx
+++ src/app/[...page]/Header.tsx
@@ -1,3 +1,4 @@
+import { Route } from 'next';
 import Link from 'next/link';
 
 import { builderSSR } from '@/builder/ssr';
--- src/app/[...page]/NavLink.tsx
+++ src/app/[...page]/NavLink.tsx
@@ -1,5 +1,6 @@
 'use client';
 
+import { Route } from 'next';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { ReactNode } from 'react';
